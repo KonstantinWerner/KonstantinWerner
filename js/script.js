d71fffc6552820ec6615630874b7d80a118ede6c
KonstantinWerner <130084592+KonstantinWerner@users.noreply.github.com>
2023-04-07 17:33:09 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*======================= toggle icon navbar ===============*/
+/*=====================toggle icon navbar===============*/
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -6,32 +6,34 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
-/*======================= scroll sections active link ===============*/
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    /*=====================Scroll active link===============*/
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop -150;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
     });
-    /*======================= sticky navbar ===============*/
+    /*=====================sitcky navbar===============*/
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    /*======================= remove toggle icon and navbar when click navbar link (scroll) ===============*/
+    /*=====================remove toggle icon and navbar when click navbar link (scroll)===============*/
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+
 };
 
 /*======================= scroll reveal ===============*/
@@ -54,4 +56,4 @@ const typed = new Typed('.multiple-text', {
     backSpeed: 100,
     backDelay: 1000,
     loop: true,
-})
+});
